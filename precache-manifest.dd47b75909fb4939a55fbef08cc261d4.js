39907b3d23b257fb3b11aff44944e36980259cd3
gtsofa <jlsnyule@yahoo.com>
2019-10-22 13:46:50 +0300

    Updates

---
+++ precache-manifest.dd47b75909fb4939a55fbef08cc261d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf823bbf8da1da9df61cc8b912203a28",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "18b3874e2a5557c90be1",
+    "url": "/monsters_rolodex/static/css/main.4862a29b.chunk.css"
+  },
+  {
+    "revision": "f2394fceff1c1f1a763a",
+    "url": "/monsters_rolodex/static/js/2.84c74688.chunk.js"
+  },
+  {
+    "revision": "18b3874e2a5557c90be1",
+    "url": "/monsters_rolodex/static/js/main.b7028ec2.chunk.js"
+  },
+  {
+    "revision": "5d48f6cc2334b9ac0bfe",
+    "url": "/monsters_rolodex/static/js/runtime-main.d39000e0.js"
+  }
+]);
